013c28c1e808da89b8b08416bf887e1c20e03d96
mbgre <mbgre@MBGLAPTOP>
2024-02-10 16:40:24 -0500

    Add project files.

---
+++ Celestial Rescale/CelestialRescale.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniLinq;
+using UnityEngine;
+using static FlightCamera;
+
+namespace Celestial_Rescale
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    internal class CelestialRescale : MonoBehaviour
+    {
+        double scaleFactor = 2;
+        float scaleFactor2 = 2;
+
+        public void Start()
+        {
+            foreach (CelestialBody body in FlightGlobals.Bodies)
+            {
+                if (body != null && body.pqsController != null)
+                {
+                    body.pqsController.ResetSphere();
+
+                    Debug.Log("[CelestialRescale] Rescaling " + body.name);
+
+                    double originalRadius = body.Radius;
+                    double targetRadius = body.Radius * scaleFactor;
+
+                    // Adjust other properties pt 1
+
+                    body.Radius *= scaleFactor;
+                    body.pqsController.radius *= scaleFactor;
+                    body.pqsController.radiusDelta *= scaleFactor;
+                    body.pqsController.radiusSquared *= scaleFactor;
+                    body.pqsController.radiusMax *= scaleFactor;
+                    body.pqsController.radiusMin *= scaleFactor;
+                    body.Mass *= scaleFactor;
+
+                    // Random stuff that probably does not work
+
+                    //body.pqsController.radiusMax *= scaleFactor;
+                    //body.pqsController.radiusMin *= scaleFactor;
+                    if (body.pqsController != null) // Additional null check
+                    {
+                        body.pqsController.mapMaxHeight *= scaleFactor;
+                    }
+
+                    foreach (PQSMod pqsMod in body.pqsController.GetComponentsInChildren<PQSMod>())
+                    {
+                        if (pqsMod != null) // Additional null check
+                        {
+                            pqsMod.RebuildSphere();
+                        }
+                    }
+
+                    if (body.name == "Kerbin" && body.isHomeWorld == true)
+                    {
+                        Debug.Log("[CelestialRescale] " + body.name +" is home world");
+                    }
+
+                    // Update the Body?
+                    body.pqsController.RebuildSphere();
+
+                    // Log the new radius (If there is a new one)
+                    if (body.Radius == originalRadius)
+                    {
+                        Debug.LogError("[CelestialRescale] Body not scaled " + body.name);
+                    }
+                    else if (body.Radius == targetRadius)
+                    {
+                        Debug.Log("[CelestialRescale] Radius Changedb" + body.Radius);
+                    }
+                    ResizeAtmosphere(body);
+                    FixScaledSpace(body);
+                    ResizeOceans(body);
+                    ResizeOrbits(body);
+                }
+            }
+        }
+
+        private void ResizeAtmosphere(CelestialBody body)
+        {
+            if (body != null && body.atmosphere == true) // Additional null check
+            {
+                body.atmosphereDepth *= scaleFactor;
+                Debug.Log("[CelestialRescale] Atmosphere Depth: " + body.atmosphereDepth);
+            }
+        }
+
+        private void ResizeOceans(CelestialBody body)
+        {
+            if (body != null && body.ocean) // Additional null check
+            {
+                Debug.Log("[CelestialRescale] Nothing Yet");
+            }
+        }
+
+        private void ResizeOrbits(CelestialBody body)
+        {
+            if (body != null && body.orbit != null && body.orbitDriver != null)
+            {
+                double originalSemiMajorAxis = body.orbit.semiMajorAxis;
+
+                body.orbit.semiMajorAxis *= scaleFactor;
+                body.orbit.UpdateFromUT(Planetarium.GetUniversalTime());
+
+                if (body.orbit.semiMajorAxis != originalSemiMajorAxis)
+                {
+                    Debug.Log("[CelestialRescale] New semi-major axis: " + body.orbit.semiMajorAxis + " " + body.name);
+                }
+                else
+                {
+                    Debug.LogError("[CelestialRescale] No change in semi-major axis" + body.name);
+                }
+            }
+        }
+
+        private void FixScaledSpace(CelestialBody body)
+        {
+            if (body != null && body.scaledBody != null) // Additional null check
+            {
+                ScaledSpaceFader[] faders = Resources.FindObjectsOfTypeAll<ScaledSpaceFader>();
+
+                foreach (ScaledSpaceFader fader in faders)
+                {
+                    if (fader != null && fader.celestialBody == body) // Additional null check
+                    {
+                        // Modify the properties of the fader as needed
+                        fader.fadeStart *= scaleFactor2;
+                        fader.fadeEnd *= scaleFactor2;
+                        break;
+                    }
+                }
+
+                body.scaledBody.transform.localScale *= scaleFactor2;
+            }
+        }
+    }
+}
---
+++ Celestial Rescale/Version.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Celestial_Rescale
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class Version : MonoBehaviour
+    {
+        public static readonly System.Version number = new System.Version("0.1.1");
+
+        public void Awake()
+        {
+            UnityEngine.Debug.Log("[CelestialRescale] Your Current Version: " + number);
+        }
+    }
+}
