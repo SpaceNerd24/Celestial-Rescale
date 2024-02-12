7952615da2efd269caaff6351f17e7c5ccec7460
SpaceNerd24 <144254541+SpaceNerd24@users.noreply.github.com>
2024-02-12 09:57:05 -0500

    Update Version.cs

--- Celestial Rescale/Version.cs
+++ Celestial Rescale/Version.cs
@@ -5,7 +5,7 @@ namespace Celestial_Rescale
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class Version : MonoBehaviour
     {
-        public static readonly System.Version number = new System.Version("0.1.2");
+        public static readonly System.Version number = new System.Version("0.1.3");
 
         public void Awake()
         {
