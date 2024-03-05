19eabe6515c33f645837c8eb89eaaf843a7e5ce2
SpaceNerd24 <randomperson100creator@gmail.com>
2024-03-05 11:41:20 -0500

    removed the stuff

--- Celestial Rescale/CelestialRescale.cs
+++ Celestial Rescale/CelestialRescale.cs
@@ -8,9 +8,10 @@ namespace Celestial_Rescale
     [KSPAddon(KSPAddon.Startup.MainMenu, true)]
     internal class CelestialRescale : MonoBehaviour
     {
-
-        public double scaleFactor = 2;
-        public float scaleFactor2 = 2;
+        float scaleFactor2 = 1;
+        double scaleFactor = 1;
+        readonly bool isDoingAtmospheres = false;
+        readonly bool usingBrokenWay = false;
 
         public void ConfigLoader()
         {
@@ -34,10 +35,6 @@ namespace Celestial_Rescale
                     {
                         Debug.Log("scaleFactor: " + value + " " + scaleFactor);
                         scaleFactor = float.Parse(value);
-                    }
-                    else if (key == "scaleFactor2" && value != null)
-                    {
-                        Debug.Log("scaleFactor2 : " + value + " " + scaleFactor2);
                         scaleFactor2 = float.Parse(value);
                     }
                     else if (value != null)
@@ -54,11 +51,15 @@ namespace Celestial_Rescale
 
         public void Start()
         {
+            AtmosphereStart();
             ConfigLoader();
+            //this is dumb
+            /*
             PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "Celestial Rescale",
             "Celestial Rescale v0.3.0",
             "Starting the Config Loader", "IDK", true, HighLogic.UISkin,
             true, string.Empty);
+            */
 
             foreach (CelestialBody body in FlightGlobals.Bodies)
             {
@@ -288,6 +289,126 @@ namespace Celestial_Rescale
                     false,
                     HighLogic.UISkin); ;
             }
+        }
+
+        void AtmosphereStart()
+        {
+            Debug.Log("did I break the game");
+            foreach (CelestialBody body in FlightGlobals.Bodies)
+            {
+                if (body != null && body.atmosphere && isDoingAtmospheres == true && usingBrokenWay == true)
+                {
+                    PrintCurve(body, "First Curves");
+
+                    CurveModifierSetup(body, "Debuging");
+
+                    PrintCurve(body, "Final Curves");
+                }
+                if (body != null && body.atmosphere && isDoingAtmospheres == true)
+                {
+                    body.atmospherePressureCurveIsNormalized = true;
+                    Debug.Log("New stuff " + body.atmospherePressureCurveIsNormalized + " Name: " + body.displayName);
+                }
+            }
+        }
+
+        ConfigNode WriteCurve(List<double[]> list)
+        {
+            ConfigNode config = new ConfigNode();
+
+            foreach (double[] values in list)
+            {
+                string key = "";
+                foreach (double value in values)
+                {
+                    key += value + " ";
+                }
+                config.AddValue("key", key);
+            }
+
+            return config;
+        }
+
+        void PrintCurve(CelestialBody body, string name)
+        {
+            Debug.Log("Doing Curve Stuff " + name + " for body " + body.name);
+            FinalCurveStuff(body.atmospherePressureCurve, "pressureCurve");
+            FinalCurveStuff(body.atmosphereTemperatureCurve, "temperatureCurve");
+            FinalCurveStuff(body.atmosphereTemperatureSunMultCurve, "temperatureSunMultCurve");
+            Debug.Log("curve1");
+        }
+
+        void PrintCurve(List<double[]> list, string name)
+        {
+            ConfigCurveStuff(WriteCurve(list), name);
+            Debug.Log("curve2");
+        }
+
+        void ConfigCurveStuff(ConfigNode config, string name)
+        {
+            FloatCurve curve = new FloatCurve();
+            curve.Load(config);
+            FinalCurveStuff(curve, name);
+            Debug.Log("curve3");
+        }
+
+        void FinalCurveStuff(FloatCurve curve, string name)
+        {
+            ConfigNode config = new ConfigNode();
+            curve.Save(config);
+            Debug.Log("CelestialRescale.AtmosphereFixer " + name);
+            Debug.Log("curve4");
+            foreach (string key in config.GetValues("key"))
+            {
+                Debug.Log("CelestialRescale.AtmosphereFixer " + "key = " + key);
+            }
+        }
+
+        void CurveModifierSetup(CelestialBody body, string name)
+        {
+            Debug.Log("Doing Curve Stuff " + name + " for body " + body.name);
+            FloatCurve newPressureCurve = MultiplyKeysAndCreateNewCurve(body.atmospherePressureCurve, "pressureCurve");
+            FloatCurve newTempCurve = MultiplyKeysAndCreateNewCurve(body.atmosphereTemperatureCurve, "temperatureCurve");
+            FloatCurve newSunCurve = MultiplyKeysAndCreateNewCurve(body.atmosphereTemperatureSunMultCurve, "temperatureSunMultCurve");
+
+            body.atmospherePressureCurve = newPressureCurve;
+            body.atmosphereTemperatureCurve = newTempCurve;
+            body.atmosphereTemperatureSunMultCurve = newSunCurve;
+            Debug.Log("curve1");
+        }
+
+        FloatCurve MultiplyKeysAndCreateNewCurve(FloatCurve curve, string name)
+        {
+            // Save the curve to a blank ConfigNode
+            ConfigNode config = new ConfigNode();
+            curve.Save(config);
+
+            // Create a new FloatCurve to store the modified values
+            FloatCurve newCurve = new FloatCurve();
+
+            // Extract all the keys from the ConfigNode
+            foreach (string key in config.GetValues("key"))
+            {
+                string[] keyParts = key.Split(',');
+                if (float.TryParse(keyParts[0], out float keyValue))
+                {
+                    // Scale the keys
+                    float multipliedValue = keyValue * scaleFactor2;
+
+                    // Generate a new curve with the scaled keys
+                    newCurve.Add(multipliedValue, float.Parse(keyParts[1]), float.Parse(keyParts[2]), float.Parse(keyParts[3]));
+                }
+            }
+
+            // Log the modified keys
+            Debug.Log("CelestialRescale.AtmosphereFixer " + name + " - Modified Keys:");
+            foreach (string key in config.GetValues("key"))
+            {
+                Debug.Log("Key: " + key);
+            }
+
+            // Return the new curve
+            return newCurve;
         }
     }
 }
